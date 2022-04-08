67b390b1b004c12364671c9c72fa0c9a270d89df
Pr4v33n23 <praveen.jaisankar@gmail.com>
2022-04-08 17:17:54 +0530

    client side code done

--- solana/index.ts
+++ solana/index.ts
@@ -159,3 +159,107 @@ export const createCampaign = async (
 
   console.log('End SendMessage', result)
 }
+
+export const getAllCampaigns = async () => {
+  let accounts = await connection.getProgramAccounts(programId)
+  let campaigns: {
+    pubId: PublicKey
+    name: string
+    description: string
+    image_link: string
+    amount_donated: number
+    admin: Buffer
+  }[] = []
+
+  accounts.forEach((account) => {
+    try {
+      let campaignData = deserialize(
+        CampaignDetails.schema,
+        // @ts-ignore
+        //! Need to fix CampaignDetails type issue.
+        CampaignDetails,
+        account.account.data
+      )
+      campaigns.push({
+        pubId: account.pubkey,
+        name: campaignData.name,
+        description: campaignData.description,
+        image_link: campaignData.image_link,
+        amount_donated: campaignData.amount_donated,
+        admin: campaignData.admin,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  return campaigns
+}
+
+export const donateToCampaign = async (campaignPubKey: any, amount: any) => {
+  await checkWallet()
+  const SEED_PRHASE = 'abcdef' + Math.random().toString()
+
+  //creating an account to contain the data of the campaign
+  let newAccount = await PublicKey.createWithSeed(
+    publicKey!,
+    SEED_PRHASE,
+    programId
+  )
+
+  const createProgramAccount = SystemProgram.createAccountWithSeed({
+    fromPubkey: publicKey!,
+    basePubkey: publicKey!,
+    seed: SEED_PRHASE,
+    newAccountPubkey: newAccount,
+    lamports: amount,
+    space: 1,
+    programId: programId,
+  })
+
+  const instructionToOurProgram = new TransactionInstruction({
+    keys: [
+      { pubkey: newAccount, isSigner: false, isWritable: true },
+      { pubkey: publicKey!, isSigner: true, isWritable: true },
+      { pubkey: campaignPubKey, isSigner: false, isWritable: true },
+    ],
+    programId: programId,
+    data: new Uint8Array([2]) as Buffer,
+  })
+}
+
+class WithDrawRequest {
+  amount: number
+
+  constructor(amount: number) {
+    this.amount = amount
+  }
+
+  static schema = new Map([
+    [WithDrawRequest, { kind: 'struct', fields: [['amount', 'u64']] }],
+  ])
+}
+
+export const withdraw = async (campaignPubKey: any, amount: number) => {
+  await checkWallet()
+  let withdrawRequest = new WithDrawRequest(amount)
+  let data = serialize(WithDrawRequest.schema, withdrawRequest)
+  let data_to_send = new Uint8Array([1, ...data])
+
+  const instructionToOurProgram = new TransactionInstruction({
+    keys: [
+      { pubkey: campaignPubKey, isSigner: false, isWritable: true },
+      { pubkey: publicKey!, isSigner: true, isWritable: true },
+    ],
+    programId: programId,
+    data: data_to_send as Buffer,
+  })
+
+  const trans = await setPayerAndBlockhashTransaction([instructionToOurProgram])
+
+  const signature = await signAndSendTransaction(trans)
+
+  const result = await connection.confirmTransaction(signature)
+
+  console.log('end sendMessage', result)
+}
